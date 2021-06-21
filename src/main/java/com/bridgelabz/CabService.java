e16050d6226b720a4cffe23721c35faffd8fde85
rehan7594 <rehansdm94@gmail.com>
2021-06-21 12:37:33 -0700

    Refactor: Code so invoice generator should return total fare for multiple rides

--- src/main/java/com/bridgelabz/CabService.java
+++ src/main/java/com/bridgelabz/CabService.java
@@ -1,8 +1,10 @@
 package com.bridgelabz;
 
 public class CabService {
+	double totalfare=0;
+
 	public double calculateFare(final double distance, final int minute) {
-		double totalfare=0;
+
 
 		if(distance==0.0 && minute==0) {
 			return totalfare=0;			
@@ -13,5 +15,12 @@ public class CabService {
 		}
 		totalfare=(distance*10) + minute;
 		return totalfare;
+	}
+
+	public double totalFareForMultipleRide(final double totalFare2, final int numOfRides) {
+
+		double totalFareForMultipleRides = totalFare2 * numOfRides;
+		return totalFareForMultipleRides;
+
 	}
 }
--- src/test/java/com/bridgelabz/CabServiceTest.java
+++ src/test/java/com/bridgelabz/CabServiceTest.java
@@ -4,10 +4,10 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class CabServiceTest {
+	CabService cabService = new CabService();
+
 	@Test
 	public void testCalculateFare_Should_Return_Total_Fare() {
-
-		CabService cabService = new CabService();
 
 		final double distance =260;
 		final int minute=90;
@@ -18,8 +18,6 @@ public class CabServiceTest {
 	@Test
 	public void testCalculateFare_Should_Return_Min_Fare() {
 
-		CabService cabService = new CabService();
-
 		final double distance =0.5;
 		final int minute=5;
 		double totalFare=cabService.calculateFare(distance,minute);
@@ -27,13 +25,21 @@ public class CabServiceTest {
 	}
 
 	@Test
-	public void testCalculateFare_Should_Return_Zero_Fare() {
-
-		CabService cabService = new CabService();
-
+	public void testCalculateFare_Should_Return_Zero_Fare() {	
 		final double distance =0.0;
 		final int minute=0;
 		double totalFare=cabService.calculateFare(distance,minute);
 		assertEquals(0, totalFare,0);
+	}
+
+	@Test
+	public void testCalculateTotalFare_Should_ReturnTotalFare_forMultipleRides() {	
+
+		final double distance =2.3;
+		final int minute=7;
+		final int numOfRides=3;
+		double totalFare=cabService.calculateFare(distance,minute);
+		double totalFareForMultipleRide=cabService.totalFareForMultipleRide(totalFare,numOfRides);
+		assertEquals(90, totalFareForMultipleRide,0);
 	}
 }
